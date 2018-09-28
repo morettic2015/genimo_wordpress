2b95452c2f18ea7d551eabc1a962ec350e76a4bf
CEO <malacma@gmail.com>
2018-09-28 15:48:27 -0300

    Share

--- genimo_wordpress/wpcasa/templates/listing-single-image.php
+++ genimo_wordpress/wpcasa/templates/listing-single-image.php
@@ -6,15 +6,15 @@ global $listing;
 
  $the_query = new WP_Query(array("post_type" => "ml-slider", "meta_key" => "_listing_slider", "meta_value" => $listing->ID));
             $cached = get_the_ID();
+            
+            echo "<!-- internal cached $cached $listing->ID-->";
+            
            if(!empty($cached)):
 ?>
 
     <meta itemprop="image" content="<?php echo esc_attr(wpsight_listing_thumbnail_url($listing->ID, 'large')); ?>" />
 
     <div class="wpsight-listing-section wpsight-listing-section-image">
-
-
-
 
         <?php do_action('wpsight_listing_single_image_before'); ?>
 
