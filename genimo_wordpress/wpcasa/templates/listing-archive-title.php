b0269c3e11b30b567a3b03b4c9323e12e52f4eb2
CEO <malacma@gmail.com>
2018-11-22 17:23:08 -0200

    Codigo do imovel no template

--- genimo_wordpress/wpcasa/templates/listing-archive-title.php
+++ genimo_wordpress/wpcasa/templates/listing-archive-title.php
@@ -1,9 +1,15 @@
 <div class="wpsight-listing-section wpsight-listing-section-title">
 	
 	<?php do_action( 'wpsight_listing_archive_title_before' ); ?>
-
+    <!-- <?php 
+        global $wpdb;
+        $post = get_post();
+        $query = "SELECT meta_value from wp_postmeta where meta_key='_listing_id' and post_id = ".$post->ID;
+        $code = $wpdb->get_var($query);
+    
+?>-->
 	<div class="wpsight-listing-title">
-	    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), ' - '.$code.'</a></h2>' ); ?>
 	</div>
 	
 	<?php do_action( 'wpsight_listing_archive_title_after' ); ?>
